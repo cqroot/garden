7df58877bd75a8a1329040686e712fa45ceb9781
cqroot <cqroot@outlook.com>
2023-01-15 12:00:51 +0800

    feat: support read config from environment variables

--- internal/configs/configs.go
+++ internal/configs/configs.go
@@ -4,10 +4,15 @@ import "github.com/spf13/viper"
 
 func init() {
 	viper.SetTypeByDefaultValue(true)
+	viper.SetEnvPrefix("TODO")
 
 	viper.SetDefault("bind_ip", "127.0.0.1")
-	viper.SetDefault("bind_port", "8000")
+	viper.SetDefault("bind_port", 8000)
 	viper.SetDefault("log_level", "Debug")
+
+	viper.BindEnv("bind_ip")
+	viper.BindEnv("bind_port")
+	viper.BindEnv("log_level")
 }
 
 func BindIp() string {
