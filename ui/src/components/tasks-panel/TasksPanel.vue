3b9bf2175b4a618e57a08a1f4572fd676369b80b
cqroot <cqroot@outlook.com>
2023-01-28 20:46:29 +0800

    feat(ui): move the button to create a task to the bottom

--- ui/src/components/tasks-panel/TasksPanel.vue
+++ ui/src/components/tasks-panel/TasksPanel.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import NewTaskPanel from "./components/NewTaskPanel.vue";
 import TaskList from "./components/TaskList.vue";
 
 const taskListRef = ref();
 const taskDialogRef = ref();
 
-const handleTaskClick = (id: number) => {
+function handleEditTask(id: number) {
   taskDialogRef.value.editTask(id);
-};
+}
 
-const updateTasks = (dueStart: number, dueEnd: number) => {
+function handleCreateTask() {
+  taskDialogRef.value.createTask();
+}
+
+function updateTasks(dueStart: number, dueEnd: number) {
   taskListRef.value.updateTasks(dueStart, dueEnd);
-};
+}
 
-const updateTask = (id: number) => {
+function updateTask(id: number) {
   taskListRef.value.updateTask(id);
-};
+}
 
 defineExpose({
   updateTasks,
@@ -25,8 +28,24 @@ defineExpose({
 </script>
 
 <template>
-  <new-task-panel @on-task-put="updateTasks" />
-  <task-list ref="taskListRef" @on-task-click="handleTaskClick" />
+  <n-layout position="absolute">
+    <n-layout position="absolute" style="bottom: 64px; padding: 24px">
+      <task-list ref="taskListRef" @on-task-click="handleEditTask" />
+    </n-layout>
+    <n-layout-footer bordered position="absolute" style="height: 64px">
+      <n-button
+        strong
+        secondary
+        type="primary"
+        class="w-full h-full rounded-none"
+        style="font-weight: bold; font-size: 15px"
+        @click="handleCreateTask"
+      >
+        Create a new task
+      </n-button>
+    </n-layout-footer>
+  </n-layout>
+
   <task-dialog
     ref="taskDialogRef"
     @on-task-create="updateTasks"
